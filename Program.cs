86a39d9144647933073e9d0cc709dbd66c2f9c19
mali053 <m9743740@gmail.com>
2024-03-07 19:28:27 +0200

    Add Token Service

--- Program.cs
+++ Program.cs
@@ -1,28 +1,83 @@
 using myChores.Middleware;
 using myChores.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.FileProviders;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddChore();
 builder.Services.AddUser();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
+
+// Authentication and Authorization
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(cfg =>
+    {
+        cfg.RequireHttpsMetadata = false;
+        cfg.TokenValidationParameters = TokenService.GetTokenValidationParameters();
+    });
+
+builder.Services.AddAuthorization(cfg =>
+{
+    cfg.AddPolicy("Admin", policy => policy.RequireClaim("type", "Admin"));
+    cfg.AddPolicy("Agent", policy => policy.RequireClaim("type", "User", "Admin"));
+});
+
+// Add Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
+app.Map("/login", builder =>
+{
+    builder.UseStaticFiles(new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+        RequestPath = "/login"
+    });
+});
 
+// Middleware
 app.UseMyLogMiddleware();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
---
+++ Services/TokenService.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace myChores.Services;
+
+public static class TokenService
+{
+    private static SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SXkSqsKyNUyvGbnHs7ke2NCq8zQzNLW7mPmHbnZZ"));
+        private static string issuer = "https://chore.com";
+        public static SecurityToken GetToken(List<Claim> claims) =>
+            new JwtSecurityToken(
+                issuer,
+                issuer,
+                claims,
+                expires: DateTime.Now.AddDays(30.0),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+            );
+
+        public static TokenValidationParameters GetTokenValidationParameters() =>
+            new TokenValidationParameters
+            {
+                ValidIssuer = issuer,
+                ValidAudience = issuer,
+                IssuerSigningKey = key,
+                ClockSkew = TimeSpan.Zero // remove delay of token when expire
+            };
+
+        public static string WriteToken(SecurityToken token) =>
+            new JwtSecurityTokenHandler().WriteToken(token);
+    }
