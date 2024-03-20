955e6d37eac98d05d43ce1f69eef139b33cee38b
mali053 <m9743740@gmail.com>
2024-03-20 23:19:46 +0200

    add connect with google

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,10 +1,10 @@
 const postmanButton = document.getElementById("postmanButton");
 
 postmanButton.addEventListener("click", function() {
-    window.open('postman://app', '_blank');
-});
+    const postmanURL = `https://www.getpostman.com/login?username=${"Admin"}&password=${"123"}`;
 
-const uri = '/login';
+    window.open(`postman://app?username=${"Admin"}&password=${"123"}`, '_blank');
+});
 
 function processTokenAndRedirect(token) {
     if (typeof token === 'object') {
@@ -24,7 +24,7 @@ function login() {
         password: password
     };
 
-    fetch(uri, {
+    fetch('/login', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
@@ -35,4 +35,68 @@ function login() {
     .then(response => response.json())
     .then(response => processTokenAndRedirect(response))
     .catch(error => console.error('Unable to add item.', error));
+}
+
+function Login(name, password) {
+    const user = {
+        name: name,
+        password: password
+    };
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(response => processTokenAndRedirect(response))
+    .catch(error => console.error('Unable to add item.', error));
+}
+
+function checkToken(){
+    if (localStorage.getItem('token')){
+        const token = localStorage.getItem('token');
+        const tokenParts = token.split('.');
+        const payload = JSON.parse(atob(tokenParts[1]));
+        const currentTime = Math.floor(Date.now() / 1000);
+
+        if(payload.exp > currentTime)
+            window.location.href = '../tasks.html'
+    }
+if (window.location.protocol === "https:") {
+    console.log("This site is served over HTTPS.");
+} else {
+    console.warn("This site is not served over HTTPS. Consider securing it with HTTPS for better security.");
+}
+
+}
+
+checkToken()
+
+function handleCredentialResponse(response)
+{
+        if (response.credential) {
+            var idToken = response.credential;
+            var decodedToken = parseJwt(idToken);
+            var userId = decodedToken.sub; // User ID
+            var userName = decodedToken.name; // User Name
+            Login(userName,userId)
+            sendTokenToServer(idToken);
+            
+        } else {
+            alert('Google Sign-In was cancelled.');
+        }
+    }
+
+    function parseJwt(token) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    
+        return JSON.parse(jsonPayload);
 }
